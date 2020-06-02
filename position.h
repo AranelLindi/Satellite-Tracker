a2d4f5ac6dd0cadf2b1467dc1a00ed565f837ca8
Stefan Lindörfer <stefan.lindoerfer@me.com>
2020-06-03 00:29:34 +0200

    Position Header und Cpp File angelegt, Operatoren global verfügbar gemacht (Prototypen) und neue Versionen der Submodule hinzugefügt.

---
+++ position.h
@@ -0,0 +1,27 @@
+#ifndef POSITION_H
+#define POSITION_H
+
+// Standartbibliotheken
+#include <iostream>
+#include <fstream>
+
+// eigener Code
+#include "Orbitpropagation/orbitpropagation.h" // für SGP4-Berechnungen
+#include "GregorianCalendar.h"                 // Calendar-Container
+
+struct Position
+{
+    // Konstanten: (Aufgabe 3.1)
+    const GregorianCalendar START = {2020, 5, 29, 14, 25, 0}; // Stellt Dummy-mäßig die Start-Zeit dar
+    const GregorianCalendar ENDE = {2020, 5, 29, 14, 32, 0};  // Stellt zum vergleichen, die End-Zeit dar
+
+    // Müssen als 'friend' definiert werden, da
+    friend bool operator!=(const GregorianCalendar &gregCal1, const GregorianCalendar &gregCal2);
+    friend GregorianCalendar &operator++(GregorianCalendar &temp, int);
+};
+
+// Prototypen: Operatoren sind damit auch in anderen Files nutzbar
+bool operator!=(const GregorianCalendar &gregCal1, const GregorianCalendar &gregCal2);
+GregorianCalendar &operator++(GregorianCalendar &temp, int);
+
+#endif // position.h
